470816e73e7ac538868422b0e59ab55971f3447d
Ninja2EatYa <alexandrov.peter@gmail.com>
2024-07-21 17:16:34 +0300

    Программистам всё можно.py

--- Программистам всё можно.py
+++ Программистам всё можно.py
@@ -1,7 +1,11 @@
 def add_everything_up(a, b):
-    return f'{str(a) + str(b)}' if isinstance(a, str) or isinstance(b, str) else round(float(f'{a + b}'), 3)
+    try:
+        print('Ура! Условие соблюдено:', round(float(f'{a + b}'), 3))
+    except TypeError as exc:
+        print(f'Условие - {exc} - не соблюдено: {str(a) + str(b)}')
 
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+
+add_everything_up(123.456, 'строка')
+add_everything_up('яблоко', 4215)
+add_everything_up(123.456, 7)
